9c6c8fb8a36dd67a8ac480a0df0ebe1c4c451a44
Ecko Huynh <nhunghuynhthihong@gmail.com>
2018-07-20 00:18:51 +0800

    This is second

--- PlayGit/ViewController.swift
+++ PlayGit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("This is first")
+        print("This is second")
     }
 
     override func didReceiveMemoryWarning() {
